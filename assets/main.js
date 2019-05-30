64d7728bfd328694a0ac4f162adae3e4885b291d
Zac Morton <zmorton8057@gmail.com>
2019-05-30 16:01:23 -0700

    Updated Education Section and Files

--- assets/main.js
+++ assets/main.js
@@ -16,7 +16,22 @@ $(document).ready(function(){
   
   TweenMax.from(".fadeInBottom", 1, {alpha:0, y:"100%"});
 
+  //Cloudinary Image Hosting
   var cloudinary = require("cloudinary-core"); // If your code is for ES5
 import cloudinary from “cloudinary-core”;    // If your code is for ES6 or higher
 
 var cl = new cloudinary.Cloudinary({cloud_name: "zacmorton-com", secure: true});
+
+//Carousel
+$('#myCarousel').on('slid.bs.carousel', '', function() {
+    var $this = $(this);
+  
+    $this.children('.carousel-control').show();
+  
+    if($('.carousel-inner .item:first').hasClass('active')) {
+      $this.children('.left.carousel-control').hide();
+    } else if($('.carousel-inner .item:last').hasClass('active')) {
+      $this.children('.right.carousel-control').hide();
+    }
+  
+  });
